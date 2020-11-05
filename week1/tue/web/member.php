ddd6f646108bc58be49c1c8dc177cd800999ce9f
Oche Ejeh <35895581+goodnessemmanuel@users.noreply.github.com>
2020-11-05 05:36:58 +0100

    add css styles to member.php page

--- week1/tue/web/member.php
+++ week1/tue/web/member.php
@@ -79,25 +79,186 @@
     <title>Member</title>
 
     <style>
-    
+        @import url(https://fonts.googleapis.com/css?family=Roboto:400,300,500);
+        *:focus {
+            outline: none;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+            background: #DDD;
+            font-size: 16px;
+            color: #222;
+            font-family: 'Roboto', sans-serif;
+            font-weight: 300;
+        }
+
+        #login-box {
+            position: relative;
+            margin: 5% auto;
+            width: 600px;
+            height: 400px;
+            background: #FFF;
+            border-radius: 2px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+        }
+
+        .left {
+            position: absolute;
+            top: 0;
+            left: 0;
+            box-sizing: border-box;
+            padding: 40px;
+            width: 350px;
+            height: 400px;
+        }
+
+        h1 {
+            margin: 0 0 20px 0;
+            font-weight: 300;
+            font-size: 28px;
+        }
+
+        input[type="text"]{
+            display: block;
+            box-sizing: border-box;
+            margin-bottom: 20px;
+            padding: 4px;
+            width: 220px;
+            height: 32px;
+            border: none;
+            border-bottom: 1px solid #AAA;
+            font-family: 'Roboto', sans-serif;
+            font-weight: 400;
+            font-size: 15px;
+            transition: 0.2s ease;
+        }
+
+        input[type="text"]:focus,
+        input[type="password"]:focus {
+            border-bottom: 2px solid #16a085;
+            color: #16a085;
+            transition: 0.2s ease;
+        }
+
+        input[type="submit"] {
+            margin-top: 28px;
+            width: 120px;
+            height: 32px;
+            background: #16a085;
+            border: none;
+            border-radius: 2px;
+            color: #FFF;
+            font-family: 'Roboto', sans-serif;
+            font-weight: 500;
+            text-transform: uppercase;
+            transition: 0.1s ease;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover,
+        input[type="submit"]:focus {
+            opacity: 0.8;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+            transition: 0.1s ease;
+        }
+
+        input[type="submit"]:active {
+            opacity: 1;
+            box-shadow: 0 1px 2px rgba(0, 0, 0, 0.4);
+            transition: 0.1s ease;
+        }
+
+        .or {
+            position: absolute;
+            top: 140px;
+            left: 300px;
+            width: 40px;
+            height: 40px;
+            background: #DDD;
+            border-radius: 50%;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+            line-height: 40px;
+            text-align: center;
+        }
+
+        .right {
+            position: absolute;
+            top: 0;
+            right: 0;
+            box-sizing: border-box;
+            padding: 40px;
+            width: 250px;
+            height: 400px;
+            background: url('https://goo.gl/YbktSj');
+            background-size: cover;
+            background-position: center;
+            border-radius: 0 2px 2px 0;
+        }
+
+        .right .loginwith {
+            display: block;
+            margin-bottom: 40px;
+            font-size: 28px;
+            color: #FFF;
+            text-align: center;
+        }
+
+        button.social-signin {
+            margin-top: auto;
+            margin-bottom: 20px;
+            width: 200px;
+            height: 36px;
+            border: none;
+            border-radius: 2px;
+            color: #FFF;
+            font-family: 'Roboto', sans-serif;
+            font-weight: 500;
+            transition: 0.2s ease;
+            cursor: pointer;
+        }
+
+        button.social-signin:hover,
+        button.social-signin:focus {
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+            transition: 0.2s ease;
+        }
+
+        button.social-signin:active {
+            box-shadow: 0 1px 2px rgba(0, 0, 0, 0.4);
+            transition: 0.2s ease;
+        }
+
+        button.social-signin.facebook {
+            position: relative;
+            top: 80px;
+            background: #32508E;
+        }
     </style>
 </head>
 <body>
     <header>
-        <h1>Membership form</h1>
+        <h1></h1>
     </header>
     <main>
-        <form method="post">
-            <label for="firstname">First Name: </label>
-            <input name="firstname" type="text" id="firstname"/>
-            <br>
-            <label for="lastname">Last Name: </label>
-            <input name="lastname" type="text" id="lastname"/>
-            <br>
-
-            <button>Submit</button>
-        </form>
-
+        <div id="login-box">
+            <form action="" method="post">
+                <div class="left">
+                    <h1>Membership Form</h1>
+                    
+                    <input type="text" name="firstname" placeholder="First Name *" />
+                    <input type="text" name="lastname" placeholder="Last Name *" />
+                    
+                    <input type="submit" value="Add Member"/>
+                </div>
+                
+                <div class="right">
+                    <button class="social-signin facebook">View Members</button>
+                </div>
+                <div class="or">OR</div>
+            </form>
+        </div>
     </main>
 </body>
 </html>
